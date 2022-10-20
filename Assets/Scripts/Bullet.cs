e02dbea74a18e8795be663b12eb30aac3520fadc
HectorVergel <hvergel@edu.tecnocampus.cat>
2022-10-20 12:03:47 +0200

    ShootingGallery++

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -10,33 +10,37 @@ public class Bullet : MonoBehaviour
     private Vector3 m_BulletDirection;
     public GameObject m_decalPrefab;
 
-    
+
     void Update()
     {
-        
+
         transform.position += m_BulletSpeed * m_BulletDirection * Time.deltaTime;
 
         RaycastHit l_raycastHit;
 
-        if (Physics.Raycast(transform.position, m_BulletDirection, out l_raycastHit, 1f, m_LayerMask.value))
+        if (Physics.Raycast(transform.position, m_BulletDirection, out l_raycastHit, 0.1f, m_LayerMask.value))
         {
-            if(l_raycastHit.collider.tag == "DroneCollider")
+            if (l_raycastHit.collider.tag == "DroneCollider")
             {
-               
+
                 l_raycastHit.collider.GetComponent<HitCollider>().Hit();
             }
-            if(l_raycastHit.collider.tag == "Player")
+            if (l_raycastHit.collider.tag == "Player")
             {
                 l_raycastHit.collider.GetComponent<PlayerHealth>().TakeDamage(m_BulletDamage);
             }
-            if(m_decalPrefab != null)
+            if (l_raycastHit.collider.tag == "ShootingElement")
+            {
+                Destroy(l_raycastHit.collider.gameObject);
+            }
+            if (m_decalPrefab != null)
             {
                 CreateShootHitParticles(l_raycastHit.collider, l_raycastHit.point, l_raycastHit.normal);
 
             }
-            Destroy(this.gameObject);
+            Destroy(this.gameObject, 1f);
         }
-           
+
     }
 
     void CreateShootHitParticles(Collider _collider, Vector3 position, Vector3 normal)
--- Assets/ShootingElement.cs
+++ Assets/ShootingElement.cs
@@ -5,7 +5,8 @@ public class ShootingElement : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Bullet")
+        
+        if (other.tag == "Bullet")
         {
             Debug.Log("in");
             gameObject.SetActive(false);
